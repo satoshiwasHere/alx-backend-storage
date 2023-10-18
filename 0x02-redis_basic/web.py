2018aff308e8eb87f4a088507e999d8d78e3b365
satoshiwasHere <skinyanjui100@gmail.com>
2023-10-18 19:44:35 +0000

    Redis is cool

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,36 +6,35 @@ HTML content of a particular URL and returns it
 import redis
 import requests
 from functools import wraps
+from typing import Callable
 
-store = redis.Redis()
 
-
-def count_url_access(method):
+def track_get_page(fn: Callable) -> Callable:
     """
     Decorator counts how many times
     a URL/page is accessed
     """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """
+        verifies if a url's data is cached
+        tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
     return wrapper
 
 
-@count_url_access
+@track_get_page
 def get_page(url: str) -> str:
     """
-    Makes a http request retuning HTML content of url 
+    Makes a http request retuning HTML content of url
     """
-    res = requests.get(url)
-    return res.text
+    response = requests.get(url)
+    return response.text
